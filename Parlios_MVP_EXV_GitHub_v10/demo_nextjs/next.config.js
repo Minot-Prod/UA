916ac07efc685d173361cd2daeda2158f726cbe8
Minot-Prod <maxime.grauliere@gmail.com>
2025-10-04 00:36:27 -0400

    Add files via upload

---
+++ Parlios_MVP_EXV_GitHub_v10/demo_nextjs/next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  i18n: {
+    locales: ['fr','en'],
+    defaultLocale: 'fr',
+  },
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
